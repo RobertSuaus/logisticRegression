de92faa0d09dcdc1812a3caf1e2324833bb9438f
RobertSuaus <roberto_ivan@outlook.es>
2016-03-12 15:46:00 -0600

    Added files via upload

---
+++ LogisticRegression/LogisticRegression/MainWindow.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace LogisticRegression
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.logBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.runToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.graph = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.testToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.graph)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // logBox
+            // 
+            this.logBox.Location = new System.Drawing.Point(12, 371);
+            this.logBox.Multiline = true;
+            this.logBox.Name = "logBox";
+            this.logBox.ReadOnly = true;
+            this.logBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.logBox.Size = new System.Drawing.Size(656, 136);
+            this.logBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(9, 355);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Log";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.runToolStripMenuItem,
+            this.toolsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(680, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.openToolStripMenuItem.Text = "Open...";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click_1);
+            // 
+            // runToolStripMenuItem
+            // 
+            this.runToolStripMenuItem.Name = "runToolStripMenuItem";
+            this.runToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.runToolStripMenuItem.Text = "Run";
+            this.runToolStripMenuItem.Click += new System.EventHandler(this.runToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // graph
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.graph.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.graph.Legends.Add(legend4);
+            this.graph.Location = new System.Drawing.Point(0, 23);
+            this.graph.Name = "graph";
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series7.Legend = "Legend1";
+            series7.Name = "Approved";
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series8.Color = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            series8.Legend = "Legend1";
+            series8.Name = "NotApproved";
+            this.graph.Series.Add(series7);
+            this.graph.Series.Add(series8);
+            this.graph.Size = new System.Drawing.Size(680, 329);
+            this.graph.TabIndex = 7;
+            this.graph.Text = "chart1";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.testToolStripMenuItem});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // testToolStripMenuItem
+            // 
+            this.testToolStripMenuItem.Enabled = false;
+            this.testToolStripMenuItem.Name = "testToolStripMenuItem";
+            this.testToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.testToolStripMenuItem.Text = "Test...";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(680, 519);
+            this.Controls.Add(this.graph);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.logBox);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "MainWindow";
+            this.Text = "Logistic Regression";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.graph)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox logBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem runToolStripMenuItem;
+        private System.Windows.Forms.DataVisualization.Charting.Chart graph;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem testToolStripMenuItem;
+    }
+}
+
---
+++ LogisticRegression/LogisticRegression/MainWindow.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace LogisticRegression
+{
+    public partial class MainWindow : Form
+    {
+        public String filePath="";
+        public double[][] initValues = new Double[100][];
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Cuando main corre ya todos los datos están inicializados
+        public void main()
+        {
+            //Graficar los valores iniciales
+            foreach (double[] data in initValues)
+            {
+                if (data[2] == 0)
+                {
+                    graph.Series["NotApproved"].Points.AddXY(data[0], data[1]);
+                }
+                else
+                {
+                    graph.Series["Approved"].Points.AddXY(data[0], data[1]);
+                }
+            }
+            log("Displaying initial values graph...");
+        }
+
+        //*****************************
+        //*****************************
+        //      Funciones para la ventana
+        
+        //Imprime a la ventana de registros
+        public void log(String str)
+        {
+            logBox.AppendText(str);
+            logBox.AppendText(Environment.NewLine);
+        }
+
+        //Solicita la ruta del archivo
+        private void openToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog choofdlog = new OpenFileDialog();
+            choofdlog.Filter = "All Files (*.*)|*.*";
+            choofdlog.FilterIndex = 1;
+            choofdlog.Multiselect = true;
+            if (choofdlog.ShowDialog() == DialogResult.OK)
+            {
+                filePath = choofdlog.FileName;
+                log("Selected file: " + filePath);
+                log("Loading data from file... Please wait");
+                readFileData();
+            }
+
+            else
+            {
+                filePath = string.Empty;
+            }
+        }
+
+        //Leer los datos y almacenarlos en un double[][]
+        private void readFileData()
+        {
+            var fileContents = System.IO.File.ReadAllLines(filePath);
+            int i = 0;
+            foreach (string line in fileContents)
+            {
+                var fields = line.Split(',');
+                if (fields.Count() != 3)
+                {
+                    MessageBox.Show("Invalid set of data", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    log("Invalid file, try again...");
+                    filePath = "";
+                    return ;
+                }
+                var test1 = double.Parse(fields[0]);
+                var test2 = double.Parse(fields[1]);
+                var result = double.Parse(fields[2]);
+                initValues[i] = new double[] { test1, test2, result };
+                i++;
+            }
+            log("Data succesfully loaded");
+            log("Ready to run!");
+        }
+
+        //Corre la aplicacion (Llama a main una vez que este todo listo)
+        private void runToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (filePath == "")
+            {
+                MessageBox.Show("You must select a file.", 
+                   "Hold it!",
+                   MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            main();
+            
+        }
+
+        //Mensaje de ayuda
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Tarea 3 de IA\nRoberto González\nCristian Xool", "Regresion Logistica",
+                MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+    }
+    
+}
